a3384fc8fdc977ef047de6de8f0b17cd98c0e9ef
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-06-07 18:12:25 +1000

    install PWA and Apple touch icon link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule, MatCardModule, MatButtonModule } from  '@angular/material';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { MatToolbarModule, MatCardModule, MatButtonModule } from  '@angular/mate
     HttpClientModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
